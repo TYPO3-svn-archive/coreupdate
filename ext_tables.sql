616b9300ab583a633f5e30d2b74269d45eb8eddb
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-25 09:30:33 +0000

    [BUGFIX] Fix case in SQL Definitions
    Avoid looping Database not uptodate messages
    
    Fixes: #41341
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/coreupdate/trunk@66335 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,11 +2,11 @@
 # TABLE STRUCTURE FOR TABLE 'tx_coreupdate_cache'
 #
 CREATE TABLE tx_coreupdate_cache (
-    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    identifier VARCHAR(250) DEFAULT '' NOT NULL,
-    crdate INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+    id int(11) unsigned NOT NULL auto_increment,
+    identifier varchar(250) DEFAULT '' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
     content mediumblob,
-    lifetime INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+    lifetime int(11) unsigned DEFAULT '0' NOT NULL,
     PRIMARY KEY (id),
     KEY cache_id (identifier)
 ) ENGINE=InnoDB;
@@ -15,9 +15,9 @@ CREATE TABLE tx_coreupdate_cache (
 # TABLE STRUCTURE FOR TABLE 'tx_coreupdate_cache_tags'
 #
 CREATE TABLE tx_coreupdate_cache_tags (
-    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    identifier VARCHAR(250) DEFAULT '' NOT NULL,
-    tag VARCHAR(250) DEFAULT '' NOT NULL,
+    id int(11) unsigned NOT NULL auto_increment,
+    identifier varchar(250) DEFAULT '' NOT NULL,
+    tag varchar(250) DEFAULT '' NOT NULL,
     PRIMARY KEY (id),
     KEY cache_id (identifier),
     KEY cache_tag (tag)
